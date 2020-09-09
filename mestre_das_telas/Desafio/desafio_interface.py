eeaea30b25f1be345015240190510a55fb088152
Marcos Lima Marinho <marcolim977@gmail.com>
2020-09-08 22:28:47 -0300

    PySimpleGUI - Refactored code for object orientation

--- mestre_das_telas/Desafio/desafio_interface.py
+++ mestre_das_telas/Desafio/desafio_interface.py
@@ -1,50 +1,59 @@
 import PySimpleGUI as sg
 import sys
 
-sg.theme('DarkBlue3')
 
-usuarios = [
-    {
-        'usuario': 'marcos',
-        'senha': '12345'
-    },
-    {
-        'usuario': 'admin',
-        'senha': 'root'
-    },
-]
+class Program:
+    def __init__(self):
+        self.usuarios = [
+            {
+                'usuario': 'marcos',
+                'senha': '12345'        
+            },
+            {
+                'usuario': 'admin',
+                'senha': 'root'
+            },       
+        ]
 
-layout = [
-    [sg.Text('Digite seu usuário')],
-    [sg.Input(key='user')],
-    [sg.Text('Digite sua senha')],
-    [sg.Input(key='pass', password_char='*')],
+    def main(self):
+        sg.theme('DarkBlue3')
+        screen_login = self.create_login_screen()
 
-    [sg.Text(key='acesso', text='Login inválido', visible=False, text_color='red')],
-    [sg.Button('Login'), sg.Button('Sair')]
-]
+        while True:
+            window, event, values = sg.read_all_windows()
 
-window = sg.Window('Janela Login', layout)
+            # Validações Tela de Login
+            if window == screen_login and event == 'Login':
+                user = values['user']
+                passw = values['pass']
 
-def exit_program():
-    window.close()
-    sys.exit()
-    
-while True:
-    event, values = window.Read()
-   
-    if event == 'Login':
-        user = values['user']
-        passw = values['pass']
+                for usuario in self.usuarios:
+                    if usuario['usuario'] == user and usuario['senha'] == passw:
+                        sg.popup('Login correto')
+                        window.close()
+                        sys.exit()
+                    else:
+                        window['acesso'].update(visible=True)
+                    
 
-        for usuario in usuarios:
-            if usuario['usuario'] == user and usuario['senha'] == passw:
-                sg.popup('Login correto')
-                exit_program()
-            else:
-                window['acesso'].update(visible=True)
-            
+            if window == screen_login and event in (sg.WIN_CLOSED, None, 'Sair'):
+                window.close()
+                sys.exit()
 
-    if event in (sg.WIN_CLOSED, None, 'Sair'):
-        exit_program()
 
+    def create_login_screen(self):
+        layout = [
+            [sg.Text('Digite seu usuário')],
+            [sg.Input(key='user')],
+            [sg.Text('Digite sua senha')],
+            [sg.Input(key='pass', password_char='*')],
+
+            [sg.Text(key='acesso', text='Login inválido', visible=False, text_color='red')],
+            [sg.Button('Login'), sg.Button('Sair')]
+        ]
+
+        return sg.Window('Janela Login', layout, finalize=True)
+
+
+program = Program()
+program.main()
